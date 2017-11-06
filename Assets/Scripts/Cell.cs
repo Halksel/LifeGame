17cfd3dde96fce52fbd6a82dbb580936dc95591f
平健太郎 <whentheycry0708@gmail.com>
2017-11-07 01:34:49 +0900

    first commit

---
+++ Assets/Scripts/Cell.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Cell : MonoBehaviour {
+	public GameObject aliveCube;
+	public GameObject deadCube;
+	private CellMgr cellMgr = CellMgr.Instance;
+
+	[SerializeField]
+	private bool isAlive;
+	public bool isPastAlive {set; get;}
+	public int x,y;
+
+	void Awake () {  
+		aliveCube.SetActive (true);  
+		deadCube.SetActive (false);  
+		isAlive = false;  
+	}
+	public void Init(int _x,int _y,bool _isAlive){
+		x = _x;
+		y = _y;
+		isAlive = _isAlive;
+		isPastAlive = _isAlive;
+		StartTurn();
+	}
+	// Use this for initialization
+	void Start () {
+				
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Input.GetMouseButtonDown(0)){
+			Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);  
+			RaycastHit hit = new RaycastHit();  
+
+			if (Physics.Raycast(ray, out hit)){  
+				Cell cell = hit.collider.gameObject.transform.parent.GetComponent<Cell>();  
+
+				if (cell.isAlive) {  
+					cell.Die ();  
+					cell.isPastAlive = false;
+				} else {  
+					cell.Birth ();  
+					cell.isPastAlive = true;
+				}  
+			}  
+		}
+	}
+		
+	public virtual bool PastTurn(){
+		Cell[,] cells = cellMgr.cells;
+		int count = 0;
+		for(int dx = -1; dx <=1 ;++dx){
+			for(int dy = -1; dy <= 1;++dy){
+				if(dx == 0 && dy == 0) continue;
+				int nx = x + dx,ny = y + dy;
+				if(cellMgr.IsValue(nx,ny) && cells[nx,ny].isPastAlive){
+					++count;
+				}
+			}
+		}
+		if(isPastAlive){ // 前ターン生存なら
+			if(count == 2 || count == 3){
+				isAlive = true;	
+			}
+			else{
+				isAlive = false;
+			}
+		}
+		else{
+			if(count == 3){
+				isAlive = true;
+			}
+		}
+		return false;
+	}
+	public void StartTurn(){
+		isPastAlive = isAlive;
+		if(isAlive){
+			Birth();			
+		}
+		else{
+			Die();
+		}
+	}
+	public void Birth() {  
+		deadCube.SetActive (false);  
+		aliveCube.SetActive (true);  
+	} 
+	public void Die() {  
+		deadCube.SetActive (true);  
+		aliveCube.SetActive (false);  
+	} 
+}
---
+++ Assets/Scripts/CellMgr.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CellMgr : SingletonMonoBehaviour<CellMgr> {
+
+	private const float CELL_SIZE = 0.5f;
+	public int xGridSize = 100;
+	public int yGridSize = 100;
+	public float turnInterval = 0.5f;
+	public Cell[,] cells;
+	public GameObject cell;
+
+ 	override protected void Awake(){
+		base.Awake();
+		Init();
+	}
+	private void Init(){
+		cells = new Cell[xGridSize,yGridSize];
+		for(int x = 0; x < xGridSize; ++x){
+			for(int y = 0; y < yGridSize;++y){
+				GameObject obj = Instantiate(cell) as GameObject;
+				obj.transform.parent = transform;
+				float xPos = (x - xGridSize * 0.5f) * CELL_SIZE;  
+				float yPos = (y - yGridSize * 0.5f) * CELL_SIZE;  
+				obj.transform.localPosition = new Vector3 (xPos, yPos,0f); 
+				cells[x,y] = obj.GetComponent<Cell>();
+				cells[x,y].Init(x,y,Random.Range(0f,100f) < 25f);
+			}
+		}
+	}
+
+	public bool IsValue(int x,int y){
+		return 0 <= x && x < xGridSize && 0 <= y && y < yGridSize;
+	}
+
+	void Update(){
+		if(Input.GetKeyDown(KeyCode.A)){
+			NextTurn();
+		}
+		if(Input.GetKeyDown(KeyCode.S)){
+			StartCoroutine(NextTurnCoroutine());
+		}
+		if(Input.GetKeyDown(KeyCode.E)){
+			StopAllCoroutines ();  
+		}
+	}
+
+	void NextTurn(){
+		for (int x = 0; x < xGridSize; ++x) {
+			for (int y = 0; y < yGridSize; ++y) {
+				cells [x, y].PastTurn ();
+				cells [x, y].StartTurn ();
+			}
+		}
+	}
+
+	IEnumerator NextTurnCoroutine(){
+		while(true){
+			NextTurn ();
+			yield return new WaitForSeconds (turnInterval);
+		}
+	}
+}
